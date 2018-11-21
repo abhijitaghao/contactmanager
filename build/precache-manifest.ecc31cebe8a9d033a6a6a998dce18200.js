cb47ed691101159fa703971cef9e8d1121b8c955
abhijitaghao <abhijitaghao@gmail.com>
2018-11-21 14:47:57 +0530

    second commit

---
+++ build/precache-manifest.ecc31cebe8a9d033a6a6a998dce18200.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "9f27e01ac7d5372abf7a",
+    "url": "/contactmanager/static/js/main.9f27e01a.chunk.js"
+  },
+  {
+    "revision": "0858eb2dc8f95e854cc7",
+    "url": "/contactmanager/static/js/1.0858eb2d.chunk.js"
+  },
+  {
+    "revision": "9f27e01ac7d5372abf7a",
+    "url": "/contactmanager/static/css/main.395b9295.chunk.css"
+  },
+  {
+    "revision": "0858eb2dc8f95e854cc7",
+    "url": "/contactmanager/static/css/1.81089745.chunk.css"
+  },
+  {
+    "revision": "f26ff90f20485ec46f57a22e589f2ab8",
+    "url": "/contactmanager/index.html"
+  }
+];
